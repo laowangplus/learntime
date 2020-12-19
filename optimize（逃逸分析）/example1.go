ea9696944c0439a2cac6e56af148a203a1b54c14
AllenWang <allenwang@xiaoe-tech.com>
2020-12-19 22:21:53 +0800

    逃逸分析

--- optimize（逃逸分析）/example1.go
+++ optimize（逃逸分析）/example1.go
@@ -3,7 +3,7 @@ package main
 // Sum 函数返回 0-100 的整数之和
 func Sum() int {
 	const count = 100
-	numbers := make([]int, count)
+	numbers := make([]int, count) //没有逃逸到堆
 	for i := range numbers {
 		numbers[i] = i + 1
 	}
@@ -14,6 +14,10 @@ func Sum() int {
 	}
 	return sum
 }
+
+/**
+因为 numbers 切片仅在 Sum函数内部使用，编译器将在栈上存储这100个整数而不是堆。也没有必要对 numbers进行垃圾回收，因为它会在 Sum 返回时自动释放。
+*/
 
 //func main()  {
 //	fmt.Println(Sum())
--- optimize（逃逸分析）/example2.go
+++ optimize（逃逸分析）/example2.go
@@ -1,29 +1,33 @@
 package main
 
 type Point struct{ X, Y int }
-type Value struct{ int }
 
 const Width = 640
 const Height = 480
 
-func Center(p *Point) {
-	p.X = Width / 2
-	p.Y = Height / 2
+type pool struct {
+	P interface{}
 }
 
-func Print(a, b *Value) int {
-	a.int, b.int = 2, 3
-	return a.int + b.int
+func Center(p Point) *Point {
+	return &p
+}
+
+func Center2(p interface{}) interface{} {
+	_ = pool{
+		P: p,
+	}
+	return &p
 }
 
 func NewPoint() {
-	p := new(Point)
+	p := Point{}
 	Center(p)
-	a, b := Value{1}, Value{1}
-	go func(a1, b1 Value) {
-		_ = Print(&a1, &b1)
-	}(a, b)
-	//fmt.Println(a,b)
+	Center2(p)
+	//fmt.Println(p)
+	//go func() {
+	//	Center(&p)
+	//}()
 }
 
 func main() {
